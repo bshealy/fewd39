71368e79d1fe8cfab229d25bd8435ace1de98bcf
bshealy <bshealy@bshealy-macbookpro3.roam.corp.google.com>
2016-01-23 11:06:42 -0800

    updated homework 4.1

--- Homework4/Homework.js
+++ Homework4/Homework.js
@@ -1,12 +1,36 @@
 for (var i = 0; i < 21; i++) {
-  console.log(i);
-if (i % 2 === 0) {console.log(i + 'is even');
+  if (i%2===0) {
+    console.log(i + " " + 'is even');
+} else {
+    console.log(i + " " + 'is odd')
+  }
 }
-else {
-  console.log(i + 'is odd')
-}}
 
 var i = 0
-while (i < 21 % 2 === 0 ) {
-  statement
+
+while (i < 21) {
+  if(i%2===0){
+    console.log(i+ " is even");
+  } else {
+    console.log(i + " is odd");
+  }
+  i++;
 }
+
+
+function max(num1, num2) {
+ if (num1 > num2) {return(num1)
+ }
+ else {
+   return(num2)
+ }
+}
+
+max(1,2);
+
+
+function maxOfThree(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+}
+
+maxOfThree (4,5,6);
